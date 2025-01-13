630a24ed740aba0b62cb3ce02d4ac200c8c0057c
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2025-01-13 12:22:25 +0900

    chore: api change

--- mod.test.ts
+++ mod.test.ts
@@ -9,7 +9,7 @@ Deno.test(function generate() {
   }])
     .withTitle("hello");
   assertEquals(
-    webpage.genhtml(),
+    webpage.genHtml(),
     `<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8" /><meta name="viewport" content="width=device-width, initial-scale=1.0" /><title>hello</title></head><body><main id="mount"></main><script type="module" src="main.js"></script></body></html>`,
   );
 });
